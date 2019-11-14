1690c8dfb4b3ef26f9e9b2f795c2b912f98ebd9f
Venryx <venryx@gmail.com>
2019-11-13 21:18:06 -0800

    * MS the alternate-approach plugin now has basic functionality. (replacements, and single-string support for includeChunk and excludeChunk)

--- Source/AlternateApproach.js
+++ Source/AlternateApproach.js
@@ -1,34 +1,25 @@
-/* var ReplaceSource;
-try {
-  ReplaceSource = require("webpack-core/lib/ReplaceSource");
-} catch(e) {
-  ReplaceSource = require("webpack-sources").ReplaceSource;
-} */
 const {ReplaceSource} = require('webpack-sources');
 
 /*
 Usage:
 ==========
-const codeMapEntries = Object.entries({
-	'const from = "complicated code from example";': 'const from = somethingSimpler;',
-});
-const ReplaceCodePlugin = require("webpack-string-replacer");
-webpackConfig.plugins.push(new ReplaceCodePlugin({
+const StringReplacerPlugin = require("webpack-string-replacer");
+webpackConfig.plugins.push(new StringReplacerPlugin({
+	chunkInclude: "chunk1",
 	replacements: {
 		"old": "new",
 	},
 }));
 */
 
-class ReplaceCodePlugin {
+class StringReplacerPlugin {
 	constructor(options) {
-		this.options = options;
+		this.options = Object.assign({chunkInclude: true, chunkExclude: false}, options);
 	}
-	options;
 	apply(compiler) {
 		compiler.plugin('compilation', (compilation) => {
 			compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
-				function getAllIndices(str, searchStr) {
+				function GetAllIndexes(str, searchStr) {
 					let i = -1;
 					const indices = [];
 					while ((i = str.indexOf(searchStr, i + 1)) !== -1) {
@@ -37,36 +28,41 @@ class ReplaceCodePlugin {
 					return indices;
 				}
 
-				chunks.forEach((chunk) => {
-					chunk.files.forEach((file) => {
-						let source;
+				for (let chunk of chunks) {
+					let {chunkInclude, chunkExclude} = this.options;
+					let definedEntries = chunk.entries;
+					const chunkNames = (definedEntries || []).map(a=>a.name);
+					if (chunkInclude == true || chunkNames.find(a=>a == chunkInclude) == null) return;
+					if (chunkExclude == true || chunkNames.find(a=>a == chunkExclude) != null) return;
+
+					for (let file of chunk.files) {
 						const originalSource = compilation.assets[file];
-
-						this.options.replacements.forEach(([fromCode, toCode]) => {
-							const indices = getAllIndices(originalSource.source(), fromCode);
-							if (!indices.length) {
-								return;
-							}
-
+						let source;
+						for (let entry of this.options.replacements) {
+							let {pattern, replacement} = entry;
+							const indices = GetAllIndexes(originalSource.source(), pattern);
+							if (!indices.length) return;
 							if (!source) {
 								source = new ReplaceSource(originalSource);
 							}
 
 							indices.forEach((startPos) => {
-								const endPos = startPos + fromCode.length - 1;
-								source.replace(startPos, endPos, toCode);
+								const endPos = startPos + pattern.length - 1;
+								source.replace(startPos, endPos, replacement);
 							});
-						});
+						}
 
 						if (source) {
-							// eslint-disable-next-line no-param-reassign
 							compilation.assets[file] = source;
 						}
 
 						callback();
-					});
-				});
+					}
+				}
 			});
 		});
 	}
 }
+
+module.exports = StringReplacerPlugin; // export as default
+module.exports.StringReplacerPlugin = StringReplacerPlugin; // export named
